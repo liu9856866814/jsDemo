7470b96fa8bc613ac6cda9ebf8b40cb3107eb09d
liuyuting <985686814@qq.com>
2019-06-07 20:51:22 +0800

    week1的一些错别字修改

---
+++ WEEK2/day4/js/2-数组复制.js
@@ -0,0 +1,17 @@
+/*
+* 目标:
+*   1. 理解数组的复制
+* */
+
+var ary = [1,2,3,4,5,];
+
+// 复制一个数组;
+// slice()
+// contact()
+// ... 扩展运算符
+var a1 = ary.slice();
+var a2 = ary.concat();
+var a3 = [...ary];
+
+
+
